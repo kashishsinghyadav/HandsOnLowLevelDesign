cce4f7fcd0c9f03f7244dd688e54d4bb65fc2234
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-12 22:41:13 +0530

    Create UpiPayment.java

---
+++ Parking Lot System Design/UpiPayment.java
@@ -0,0 +1,15 @@
+public class UpiPayment implements  PaymentStrategy{
+    private String upiId;
+
+    public UpiPayment(String upiId) {
+        this.upiId = upiId;
+    }
+
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " via UPI.");
+    }
+
+    
+}
