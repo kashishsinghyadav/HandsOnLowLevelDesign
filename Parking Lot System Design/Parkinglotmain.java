c13714d44c802d462d59898ae984a11beeb53a6e
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-12 22:42:37 +0530

    add payment and fare calculation logic

--- Parking Lot System Design/Parkinglotmain.java
+++ Parking Lot System Design/Parkinglotmain.java
@@ -1,7 +1,9 @@
 public class Parkinglotmain {
     public static void main(String[] args) {
+        //main function
        Vehicle car = VehicleFactory.creatVehicle("car");
-       
+
+        // 
     }
 
 }
