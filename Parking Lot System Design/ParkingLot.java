e415cad446743c7aecc7ddf2c01149b7acff67ea
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-13 11:27:22 +0530

    make single instance to ensure the smooth entry and exit

--- Parking Lot System Design/ParkingLot.java
+++ Parking Lot System Design/ParkingLot.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
 public class ParkingLot {
     private static ParkingLot instance;
     private List<ParkingFloor> floors = new ArrayList<>();
