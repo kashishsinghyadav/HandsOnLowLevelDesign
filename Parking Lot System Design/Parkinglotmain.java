4f1814114c0953a347a772d2e3f699e7bb8caa80
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-13 11:20:52 +0530

    Delete Parking Lot System Design/Parkinglotmain.java

--- Parking Lot System Design/Parkinglotmain.java
+++
@@ -1,9 +0,0 @@
-public class Parkinglotmain {
-    public static void main(String[] args) {
-        //main function
-       Vehicle car = VehicleFactory.creatVehicle("car");
-
-        // 
-    }
-
-}
