cfa2133e1a22d919ba0becea621d3165c17cd6f0
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-18 10:42:51 +0530

    Create VendingMachine.java

---
+++ Vending Machine Design/VendingMachine.java
@@ -0,0 +1,7 @@
+public interface VendingMachine {
+    void idlestate(Vedingcontext context);
+    void selectProduct(Vedingcontext context);
+    void makepayment(Vedingcontext context);
+    void dispensory(Vedingcontext context);
+
+}
