17c2901b4f91be8344a9400b5c0889e92331b219
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-18 10:44:35 +0530

    Create Product.java

---
+++ Vending Machine Design/Product.java
@@ -0,0 +1,31 @@
+public abstract class Product {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void reduceQuantity(int count) {
+        this.quantity -= count;
+    }
+
+    public void addQuantity(int count) {
+        this.quantity += count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
