ba3e23675ec34d749504c693de6054456ab7f577
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-24 21:38:10 +0530

    Create BookFactory.java

---
+++ LibraryManagementSystem/BookFactory.java
@@ -0,0 +1,13 @@
+public class BookFactory{
+    public static Book createBook(String bookname){
+        if(bookname.equalsIgnoreCase("Godan")){
+            return new Godan(bookname);
+        }
+        else if(bookname.equalsIgnoreCase("NumberofUniverseis42")){
+            return new NumberofUniverseis42(bookname);
+        }
+        else{
+            return null;
+        }
+        }   
+     }
