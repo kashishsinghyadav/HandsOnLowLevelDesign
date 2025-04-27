a1a9b76e93da0e2d0001edcf076183aaea0139be
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-04-27 20:33:34 +0530

    Update Lmain.java

--- LibraryManagementSystem/Lmain.java
+++ LibraryManagementSystem/Lmain.java
@@ -1,6 +1,23 @@
-public class Lmain{
+public class Lmain {
     public static void main(String[] args) {
-        Book book = BookFactory.createBook("Godan");
+        LibraryManager library = new LibraryManager();
+
+        Book scienceBook = BookFactory.createBook("science", "S1", "Physics for Beginners");
+        scienceBook.addCopy(new BookCopy("S1-C1"));
+        scienceBook.addCopy(new BookCopy("S1-C2"));
+        library.addBook(scienceBook);
+
+        Book fictionBook = BookFactory.createBook("fiction", "F1", "Harry Potter");
+        fictionBook.addCopy(new BookCopy("F1-C1"));
+        library.addBook(fictionBook);
+
+        Student student1 = new Student(1, "Kashish");
+        library.addStudent(student1);
+
+        library.issueBook("S1", 1);
+        library.issueBook("F1", 1);
+
         
+        library.returnBook("S1-C1", 1);
     }
 }
