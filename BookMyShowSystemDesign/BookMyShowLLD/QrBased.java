7d13f29234dfdaae89c49106e9e9ab7f8dbc64b6
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-05-14 08:19:13 +0530

    Update QrBased.java

--- BookMyShowSystemDesign/BookMyShowLLD/QrBased.java
+++ BookMyShowSystemDesign/BookMyShowLLD/QrBased.java
@@ -1,6 +1,6 @@
 public class QrBased implements  Payment {
     @Override
-    public void pay(int amount) {
+    public void pay(double amount) {
         System.out.print("Thankyou for making Payment" + amount);
     }
     
