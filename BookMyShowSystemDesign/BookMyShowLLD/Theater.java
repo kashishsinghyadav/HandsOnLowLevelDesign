fe7c3be8a085197cc8b68e03ca59c37b06668c0f
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-05-14 08:20:25 +0530

    Update Theater.java

--- BookMyShowSystemDesign/BookMyShowLLD/Theater.java
+++ BookMyShowSystemDesign/BookMyShowLLD/Theater.java
@@ -3,21 +3,25 @@ import java.util.List;
 
 public class Theater {
     private String theaterName; 
-    private List<Seat> thList;  
+    private List<Seat> seats;  
 
-    // Constructor
     public Theater(String theaterName) {
         this.theaterName = theaterName;
-        this.thList = new ArrayList<>(); 
+        this.seats = new ArrayList<>(); 
     }
 
-    // Getter method for theater name
     public String getTheaterName() {
         return this.theaterName;
     }
 
     // Method to add a seat to the list
     public void addSeat(Seat seatInfo) {
-        thList.add(seatInfo);
+        seats.add(seatInfo);
     }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
 }
+
