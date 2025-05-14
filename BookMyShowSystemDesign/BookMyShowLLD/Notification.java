e6f36c959d06426cdc705fca964f3645628eb441
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-05-14 08:18:01 +0530

    Update Notification.java

--- BookMyShowSystemDesign/BookMyShowLLD/Notification.java
+++ BookMyShowSystemDesign/BookMyShowLLD/Notification.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Notification {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
  
