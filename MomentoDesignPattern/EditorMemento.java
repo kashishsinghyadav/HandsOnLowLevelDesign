9d353773deca55678c71129b889341982effdbe1
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-07-07 22:05:00 +0530

    Create EditorMemento.java

---
+++ MomentoDesignPattern/EditorMemento.java
@@ -0,0 +1,11 @@
+class EditorMemento {
+    private final String content;
+
+    public EditorMemento(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
