f81eb51a9cb8733db8ec48035f75e15f6fc05933
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-07-11 16:35:44 +0530

    Create BaseEditor.java

---
+++ DecoratorDesginPattern/TextEditorwithFormattingFeature/BaseEditor.java
@@ -0,0 +1,9 @@
+package TextDecorator;
+
+public class BaseEditor implements Editor{
+    @Override
+    public void styleText() {
+        System.out.println("Common Text Styple is applied");
+
+    }
+}
