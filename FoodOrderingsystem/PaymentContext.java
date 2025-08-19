49395b831bf7cff215ed0d2d7c9de250fa0e11bf
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-19 22:27:09 +0530

    Create PaymentContext.java

---
+++ FoodOrderingsystem/PaymentContext.java
@@ -0,0 +1,11 @@
+class PaymentContext {
+    private PaymentStrategy strategy;
+
+    public PaymentContext(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executePayment(double amount) {
+        strategy.pay(amount);
+    }
+}
