b4d123e5becb22f5cbdb54a634e1bb6fb3a74b3c
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-19 22:28:13 +0530

    Create OrderItem.java

---
+++ FoodOrderingsystem/OrderItem.java
@@ -0,0 +1,11 @@
+class OrderItem {
+    private String itemName;
+    private int quantity;
+    private double price;
+
+    public OrderItem(String itemName, int quantity, double price) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
