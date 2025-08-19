11e64311b00c94931055f956f4adec9d9760c6f9
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-19 22:30:56 +0530

    Create CatalogService.java

---
+++ FoodOrderingsystem/CatalogService.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class CatalogService {
+    private InMemoryDatabase db = InMemoryDatabase.getInstance();
+
+    public List<Item> searchItem(String restaurantId, String itemName) {
+        List<Item> items = db.getCatalogs().getOrDefault(restaurantId, new ArrayList<>());// if not item return []list(so on null pointer exception)
+        List<Item> results = new ArrayList<>();
+        for (Item i : items) {
+            if (i.getName().equalsIgnoreCase(itemName)) {
+                results.add(i);
+            }
+        }
+        results.sort(Comparator.comparingDouble(Item::getPrice));
+        return results;
+    }
+}
