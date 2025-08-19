5f3656974f1cb0725399d02fbe138f2b4a8ff7fb
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-19 22:17:22 +0530

    Create UPIPayment.java

---
+++ FoodOrderingsystem/UPIPayment.java
@@ -0,0 +1,12 @@
+class UPIPayment implements PaymentStrategy {
+    private String upiId;
+
+    public UPIPayment(String upiId) {
+        this.upiId = upiId;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using UPI ID " + upiId);
+    }
+}
