774b3720e6f83a9fb58af9976101119fa9025bf2
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-25 22:48:53 +0530

    Update CashOnDeliveryPayment.java

--- FoodOrderingsystem/CashOnDeliveryPayment.java
+++ FoodOrderingsystem/CashOnDeliveryPayment.java
@@ -2,5 +2,5 @@ class CashOnDeliveryPayment implements PaymentStrategy {
     @Override
     public void pay(double amount) {
         System.out.println("Cash on Delivery chosen. Amount to pay: ₹" + amount);
-    }
+    } 
 } 
