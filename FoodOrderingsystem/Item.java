3e416e045dd7675ae15de54b77eee2c29ea097ff
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-08-25 22:49:10 +0530

    Update Item.java

--- FoodOrderingsystem/Item.java
+++ FoodOrderingsystem/Item.java
@@ -2,7 +2,7 @@ class Item {
     private String name;
     private double price;
     private int quantity;
-
+ 
     public Item(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
