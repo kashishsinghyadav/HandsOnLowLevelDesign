5845fd69807e8e1bb119f7430908e14cbd5d2067
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-09-06 05:27:11 +0530

    Update UserFactory.java

--- FoodOrderingsystem/UserFactory.java
+++ FoodOrderingsystem/UserFactory.java
@@ -1,4 +1,4 @@
-class UserFactory {
+ class UserFactory {
     public static User createUser(String name, String email, String phone) {
         return new User(IdFactory.generateId(), name, email, phone);
     }
