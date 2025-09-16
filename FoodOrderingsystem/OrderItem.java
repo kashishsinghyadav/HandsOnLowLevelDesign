b990d63377820a79c791986c7b5fc314a97b67d3
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-09-16 06:35:38 +0530

    Update OrderItem.java

--- FoodOrderingsystem/OrderItem.java
+++ FoodOrderingsystem/OrderItem.java
@@ -6,6 +6,6 @@
     public OrderItem(String itemName, int quantity, double price) {
         this.itemName = itemName;
         this.quantity = quantity;
-        this.price = price;
+        this.price = price; 
     }
 }
