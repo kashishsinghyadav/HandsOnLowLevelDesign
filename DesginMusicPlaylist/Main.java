ab85f20314d3e2f2f828d97a0ddff516df19c84d
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-10-05 02:24:17 +0530

    Update Main.java

--- DesginMusicPlaylist/Main.java
+++ DesginMusicPlaylist/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) { 
         Song song1 = new Song();
         song1.setSongName("Harry Potter Theme");
-        song1.setArtistName("John Williams");
+        song1.setArtistName("John Williams"); 
 
         Song song2 = new Song();
         song2.setSongName("Shape of You");
